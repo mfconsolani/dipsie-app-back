04a05e928ae0ada70f4bbb884fcf2f5bb21195f4
MAURO <mfconsolani@outlook.com>
2021-05-16 19:53:01 +0200

    building interview endpoint

---
+++ src/routes/interviewRoutes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+export const interviewRouter = Router()
+
+interviewRouter.get('/', (req, res) => {
+    res.send('Interview screen')
+});
+
+interviewRouter.post('/', (req, res) => {
+    const { fullname, ...rest} = req.body
+    console.log(fullname, rest);
+    res.status(200).json({'status':`info recieved: ${fullname} and ${...rest}`})
+})
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,21 @@
 import express from "express"
+import { interviewRouter } from './routes/interviewRoutes';
+
+
+
+// App Config
 
 const app = express();
 
+// Middleware
 
+app.use(express.json())
+app.use('/interview', interviewRouter)
+
+
+// Database Config
+
+// Server Config
 
 const server = app.listen(8080, () => {
     console.log("App running on port 8080")
