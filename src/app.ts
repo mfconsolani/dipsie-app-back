73304293f37d00c4147f5fdb4298aee1464b23fd
{mfconsolani} <mfconsolani@gmail.com>
2021-10-03 13:55:57 +0200

    fixed cors

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,9 @@ const app = express();
 // Middleware
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:3000/'
+}))
 app.use('/interview', interviewRouter)
 app.use('/user', userRouter)
 
