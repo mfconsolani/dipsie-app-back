54a15aad857933f1c1445a2ffd1d044bfcc5c3a3
{mfconsolani} <mfconsolani@gmail.com>
2021-10-03 14:05:34 +0200

    trying to fix cors problem

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,7 @@ const app = express();
 // Middleware
 
 app.use(express.json())
-app.use(cors({
-  origin: 'http://localhost:3000/'
-}))
+app.use(cors())
 app.use('/interview', interviewRouter)
 app.use('/user', userRouter)
 
