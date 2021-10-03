c889b486af03c412a98ea95786bf65ff992577f7
{mfconsolani} <mfconsolani@gmail.com>
2021-10-03 20:47:48 +0200

    adding try catch block

--- src/controllers/getCandidate.controller.ts
+++ src/controllers/getCandidate.controller.ts
@@ -5,11 +5,16 @@ const getCandidate = async (req: Request, res: Response) => {
     // @ts-ignore
     const userEmail = req.user[`${process.env.AUTH0_AUDIENCE}/email`];
     const { idCandidato } = req.params;
-    const infoCandidato = await findCandidate(userEmail, idCandidato)
 
-    infoCandidato.length !== 0
-      ? res.status(200).json({ Candidato: infoCandidato })
-      : res.status(404).json({ Candidato: "Candidato no encontrado" });
+    try {
+      const infoCandidato = await findCandidate(userEmail, idCandidato)
+      infoCandidato.length !== 0
+        ? res.status(200).json({ Candidato: infoCandidato })
+        : res.status(404).json({ Candidato: "Candidato no encontrado" });
+    } catch (err){
+      res.send(err);
+    }
+
   }
 
 export default getCandidate 
