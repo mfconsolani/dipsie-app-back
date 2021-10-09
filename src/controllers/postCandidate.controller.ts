293e8e6815683a4357656424fdacac8d15e190e2
{mfconsolani} <mfconsolani@gmail.com>
2021-10-09 15:54:16 +0200

    refactored postCandidate controller

--- src/controllers/postCandidate.controller.ts
+++ src/controllers/postCandidate.controller.ts
@@ -11,29 +11,23 @@ const postCandidate = async (req: Request, res: Response) => {
   // @ts-ignore
   const userEmail = req.user[`${process.env.AUTH0_AUDIENCE}/email`];
   let { candidate, id, info } = req.body;
-  const candidateInDb = await isCandidateInUser(userEmail, id);
-
-  if (candidateInDb.length > 0) {
-    try {
-      const candidateUpdate = await updateCandidateInfo(userEmail, req.body);
-      res
-        .status(200)
-        .json({ "Candidate update succedded": { candidate, id, info } });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({ Error: err });
+  
+  try {
+      const candidateInDb = await isCandidateInUser(userEmail, id);
+      if (candidateInDb.length > 0) {
+          const candidateUpdate = await updateCandidateInfo(userEmail, req.body);
+          res
+            .status(200)
+            .json({ "Candidate update succedded": { candidate, id, info } });
+      } else {
+          const candidateCreated = await createNewCandidate(req.body);
+          // evaluar si es mejor usar upsert
+          const candidateSavedToDb = await pushCandidateIntoUser(userEmail,candidateCreated);
+          res.status(200).json({ "Candidate created": { candidate, id } });
+      }
+    } catch (err){
+      res.status(500).send(err)
     }
-  } else {
-    try {
-      const candidateCreated = await createNewCandidate(req.body);
-      // evaluar si es mejor usar upsert
-      const candidateSavedToDb = await pushCandidateIntoUser(userEmail,candidateCreated);
-      res.status(200).json({ "Candidate created": { candidate, id } });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({ Error: err });
-    }
-  }
 };
 
 export default postCandidate
